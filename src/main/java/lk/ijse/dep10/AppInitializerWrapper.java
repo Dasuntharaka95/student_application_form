77c199d53ec86b1930730b064183389481b548cc
Dasun Tharaka <wkdasunt0001@gmail.com>
2023-06-23 16:46:55 +0530

    finish student aplication app

---
+++ src/main/java/lk/ijse/dep10/AppInitializerWrapper.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep10;
+
+import java.sql.SQLException;
+
+public class AppInitializerWrapper {
+
+    public static void main(String[] args) throws SQLException {
+        AppInitializer.main(args);
+    }
+}
---
+++ src/main/java/lk/ijse/dep10/db/DBConnection.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep10.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://dep10.lk:3306/dep10_jdbc", "root", "1995");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static DBConnection getInstance() {
+        return (dbConnection == null) ? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
